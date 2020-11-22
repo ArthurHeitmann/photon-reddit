d82d8f72ed3562817235e05e0504b872782171f4
ArthurHeitmann <art.tec.15@gmail.com>
2020-11-22 09:14:04 +0100

    added sub info to wiki pages

--- src/static/scripts/components/wiki/wiki.ts
+++ src/static/scripts/components/wiki/wiki.ts
@@ -1,6 +1,9 @@
-import { linksToInlineImages, linksToSpa } from "../../utils/htmlStuff.js";
+import { elementWithClassInTree, linksToInlineImages, linksToSpa } from "../../utils/htmlStuff.js";
 import { RedditApiType } from "../../utils/types.js";
 import { replaceRedditLinks } from "../../utils/utils.js";
+import Ph_FeedInfo from "../feed/feedInfo/feedInfo.js";
+import { FeedType } from "../feed/universalFeed/universalFeed.js";
+import { Ph_ViewState } from "../viewState/viewState.js";
 
 export default class Ph_Wiki extends HTMLElement {
 	constructor(wikiData: RedditApiType) {
@@ -12,6 +15,17 @@ export default class Ph_Wiki extends HTMLElement {
 		replaceRedditLinks(this);
 		linksToSpa(this);
 		linksToInlineImages(this);
+	}
+
+	connectedCallback() {
+		const subredditPath = location.pathname.match(/\/r\/[^/]+/)[0];
+		const headerElements: HTMLElement[] = [];
+		const title = document.createElement("div");
+		title.className = "feedTitle";
+		title.innerText = subredditPath;
+		headerElements.push(title);
+		headerElements.push(new Ph_FeedInfo(FeedType.subreddit, subredditPath).makeShowInfoButton());
+		(elementWithClassInTree(this.parentElement, "viewState") as Ph_ViewState).setHeaderElements(headerElements);
 	}
 }
 
