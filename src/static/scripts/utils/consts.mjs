229188a4bd2b582777147b48ade599e479cb0439
Arthur Heitmann <art.tec.15@gmai.com>
2020-12-18 18:41:58 +0000

    deployed version

--- src/static/scripts/utils/consts.mjs
+++ src/static/scripts/utils/consts.mjs
@@ -1,3 +1,2 @@
 export const appId = "N3bHM42Rmlc3sQ";
-export const redirectURI = "http://localhost:8080/redirect";
-export const mainURL = "localhost:8080";
+export const redirectURI = "https://photon-reddit.oa.r.appspot.com/redirect";
