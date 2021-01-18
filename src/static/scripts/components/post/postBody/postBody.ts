1096e4e3697ea4c8ffb52dae4f84592d98241aeb
ArthurHeitmann <art.tec.15@gmail.com>
2021-01-18 22:11:19 +0100

    fixed some twitch clips being embedded & failing, when there is no postData.data.media.oembed

--- src/static/scripts/components/post/postBody/postBody.ts
+++ src/static/scripts/components/post/postBody/postBody.ts
@@ -104,11 +104,14 @@ export default class Ph_PostBody extends HTMLElement {
 		else if (new RegExp(
 			"^((https?://(i|m)?\.?imgur\\.com\/[\\w-]+.(gifv|mp4))|" +
 			"(https?://v.redd.it\/[\\w-]+)|" +
-			"(https?://clips.twitch.tv\/[\\w-]+)|" +
 			"(https?://w?w?w?\.?redgifs.com\/watch\/\\w+))|" +
 			"(\.(gif|mp4)(\\?.*)?$)"
 		).test(postData["url"])
-		|| /^https?:\/\/gfycat.com\/[\w-]+/.test(postData["url"]) && postData["media"])
+		)
+			return PostType.Video;
+		else if (/https?:\/\/gfycat.com\/[\w-]+/.test(postData["url"]) && postData["media"])
+			return PostType.Video;
+		else if (/https?:\/\/clips.twitch.tv\/[\w-]+/.test(postData["url"]) && postData["media"])
 			return PostType.Video;
 		else if (postData["post_hint"] == "image" ||
 			/\.(png|jpg|jpeg|svg)$/.test(postData["url"]) ||
