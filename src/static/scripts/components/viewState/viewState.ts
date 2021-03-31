50161208e9bf02a092d21e70c86b294af4a67b08
ArthurHeitmann <art.tec.15@gmail.com>
2021-03-31 20:42:26 +0200

    added first window event listener cleanup

--- src/static/scripts/components/viewState/viewState.ts
+++ src/static/scripts/components/viewState/viewState.ts
@@ -1,13 +1,14 @@
 import { $tag } from "../../utils/htmlStatics.js";
 import { HistoryState } from "../../types/misc.js";
 import Ph_Header from "../global/header/header.js";
+import Ph_PhotonBaseElement from "../photon/photonBaseElement/photonBaseElement.js";
 
 /**
  * A page that is associated with a history state/url.
  * Will be displayed when viewing that history state.
  * When leaving this page, it will be hidden, not removed or unloaded.
  */
-export abstract class Ph_ViewState extends HTMLElement {
+export abstract class Ph_ViewState extends Ph_PhotonBaseElement {
 	state: HistoryState;
 	headerElements: HTMLElement[] = [];
 	header: Ph_Header;
@@ -19,6 +20,12 @@ export abstract class Ph_ViewState extends HTMLElement {
 
 		this.state = state;
 		this.header = $tag("ph-header")[0] as Ph_Header;
+
+		this.addEventListener("ph-removed", () => {
+			const cleanupElements = this.$css("[requiresCleanup]") as HTMLCollectionOf<Ph_PhotonBaseElement>;
+			for (const elem of cleanupElements)
+				elem.cleanup();
+		});
 	}
 
 	setHeaderElements(elements: HTMLElement[]) {
