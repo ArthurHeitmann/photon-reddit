426883e73f0e0d34f151c7abdf9b5332629e82b7
ArthurHeitmann <art.tec.15@gmail.com>
2021-04-04 20:09:05 +0200

    bumped version to 0.2.0

--- src/serverScripts/version.ts
+++ src/serverScripts/version.ts
@@ -1,6 +1,15 @@
-export const photonVersion = "0.1.3";		/// <change version script>
+export const photonVersion = "0.2.0";		/// <change version script>
 
 export const photonChangelog = {
+	"0.2.0": {
+		"New": [
+			"Reworked image & video viewer",
+			"Reddit + imgur galleries now also support videos",
+			"Reworked inline media",
+			"All image, video, imgur, etc. links in comments or text posts can now be directly displayed",
+			"Some other changes and fixes",
+		]
+	},
 	"0.1.3": {
 		"New": [
 			"Improved comments loading by instantly displaying already loaded post",
