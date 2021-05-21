f6f145ede2f303c3da4bdc23387eea4a2b878e32
ArthurHeitmann <art.tec.15@gmail.com>
2021-05-21 11:33:28 +0200

    fixed potential future xss injection (no " ' " were used anywhere, so wasn't a problem)

--- src/static/scripts/utils/htmlStatics.ts
+++ src/static/scripts/utils/htmlStatics.ts
@@ -93,7 +93,9 @@ export function escHTML(unsafeHTML: string): string {
 export function escADQ(unsafeAttribute: string): string {
 	const dummy = document.createElement("div");
 	dummy.setAttribute("data-escape", unsafeAttribute);
-	return dummy.outerHTML.match(/(?<=").*(?=")/s)[0];	// any text between the first " and last "
+	let out = dummy.outerHTML.match(/(?<=").*(?=")/s)[0];	// any text between the first " and last "
+	out = out.replace(/'/g, "&apos;");			// ' --> &apos;
+	return out;
 }
 
 export function getLoadingIcon(): HTMLImageElement {
