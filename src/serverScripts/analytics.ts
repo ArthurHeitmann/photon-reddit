48de62562ae7f8e5149945f1997f1cdc7a94ffae
ArthurHeitmann <art.tec.15@gmail.com>
2021-05-23 10:36:11 +0200

    added some types

--- src/serverScripts/analytics.ts
+++ src/serverScripts/analytics.ts
@@ -172,7 +172,7 @@ async function trackMediaHost(hosts: MediaHost[]): Promise<void> {
 analyticsRouter.post("/event", RateLimit(analyticsRateLimitConfig), safeExcAsync(async (req, res) => {
 	let { clientId, path, referer, timeMillisUtc } = req.body;
 	if (!path)
-		path = "/"
+		path = "/";
 	if (!clientId || typeof clientId !== "string" || clientId.length > 128) {
 		res.status(400).json({ error: "invalid parameters" });
 		return;
--- src/serverScripts/consts.ts
+++ src/serverScripts/consts.ts
@@ -1,4 +1,5 @@
 import { config } from "dotenv";
+import { Options as rlOptions } from "express-rate-limit";
 
 export const env = process.env.NODE_ENV || "development";
 if (env !== "production")
@@ -6,30 +7,23 @@ if (env !== "production")
 
 export const port = process.env.PORT || 8080;
 export const __dirname = process.cwd();
-export const tokenDuration = "permanent";
-export const scope = ["account", "creddits", "edit", "flair", "history", "identity", "livemanage", "modconfig", "modcontributors", "modflair", "modlog", "modmail", "modothers", "modposts", "modself", "modtraffic", "modwiki", "mysubreddits", "privatemessages", "read", "report", "save", "structuredstyles", "submit", "subscribe", "vote", "wikiedit", "wikiread"];
 
 // Configure middlewares
-export const commonRateLimitConfig = {
+export const commonRateLimitConfig: rlOptions = {
 	message: "A little fast hugh?",
 	headers: false
 }
-export const basicRateLimitConfig = {
+export const basicRateLimitConfig: rlOptions = {
 	windowMs: 30 * 1000,		// in a timeframe of 30s
 	max: 30,					// 30 requests are allowed
 	...commonRateLimitConfig
 };
-export const redditTokenRateLimitConfig = {
-	windowMs: 60 * 1000,
-	max: 5,
-	...commonRateLimitConfig
-};
-export const analyticsRateLimitConfig = {
+export const analyticsRateLimitConfig: rlOptions = {
 	windowMs: 40 * 1000,
 	max: 15,
 	...commonRateLimitConfig
 };
-export const youtube_dlRateLimitConfig = {
+export const youtube_dlRateLimitConfig: rlOptions = {
 	windowMs: 30 * 1000,
 	max: 40,
 	...commonRateLimitConfig
