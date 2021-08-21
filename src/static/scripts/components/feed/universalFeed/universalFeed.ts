e93d59455291214ffd873641bc4f7cd0f0800d53
ArthurHeitmann <art.tec.15@gmail.com>
2021-08-21 13:27:30 +0200

    fixed error message in console

--- src/static/scripts/components/feed/universalFeed/universalFeed.ts
+++ src/static/scripts/components/feed/universalFeed/universalFeed.ts
@@ -421,7 +421,9 @@ export default class Ph_UniversalFeed extends HTMLElement {
 	}
 
 	onResize(entries: ResizeObserverEntry[]) {
-		if (window.innerWidth === this.lastWindowSize[0] && window.innerHeight === this.lastWindowSize[1] || entries[0].contentBoxSize?.[0].blockSize === 0)
+		if (this.children.length <= 1
+			||window.innerWidth === this.lastWindowSize[0] && window.innerHeight === this.lastWindowSize[1]
+			|| entries[0].contentBoxSize?.[0].blockSize === 0)
 			return;
 		this.lastWindowSize = [window.innerWidth, window.innerHeight];
 		const scrollTopDiff = this.currentlyVisibleItem.getBoundingClientRect().top - this.currentlyVisibleItemScrollTop;
