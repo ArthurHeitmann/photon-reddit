ce8fa76165339e486a418d7ea84d7cda3d4fa6eb
ArthurHeitmann <art.tec.15@gmail.com>
2021-09-21 07:04:36 +0200

    updated push script

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
+git checkout stableBackup
+git merge master
 git checkout master
 git merge development
 git checkout deployed
 git merge master
-git push origin --all
 git checkout development
+git push origin --all
