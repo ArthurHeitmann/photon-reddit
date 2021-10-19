6d2287bf74a3731d5240f191870234425c0c0b6c
ArthurHeitmann <art.tec.15@gmail.com>
2021-10-19 13:48:50 +0200

    added LS cache to IDB check, to improve startup performance

--- src/static/scripts/utils/browserFeatures.ts
+++ src/static/scripts/utils/browserFeatures.ts
@@ -2,10 +2,15 @@
 let _isIdbSupported: boolean = null;
 export function supportsIndexedDB(): Promise<boolean> {
 	return new Promise((resolve) => {
+		// try load cached from localstorage
+		if (_isIdbSupported === null && ["true", "false"].includes(localStorage.idbSupported))
+			_isIdbSupported = localStorage.idbSupported === "true";
+		// try return memory cached version
 		if (_isIdbSupported !== null) {
 			resolve(_isIdbSupported);
 			return;
 		}
+		// test if supported
 		if (!indexedDB) {
 			resolve(_isIdbSupported = false);
 			return;
@@ -13,9 +18,11 @@ export function supportsIndexedDB(): Promise<boolean> {
 		const db = indexedDB.open("featureTest");
 		db.onsuccess = () => {
 			indexedDB.deleteDatabase("featureTest");
+			localStorage.setItem("idbSupported", "true");
 			resolve(_isIdbSupported = true);
 		};
 		db.onerror = () => {
+			localStorage.setItem("idbSupported", "false");
 			resolve(_isIdbSupported = false);
 		};
 	});
