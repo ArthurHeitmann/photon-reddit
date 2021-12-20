56e8b4de153947323fe547b26c8ad23eaa27d34e
ArthurHeitmann <art.tec.15@gmail.com>
2021-12-20 18:05:46 +0100

    fixed popout videos

--- src/static/scripts/components/mediaViewer/videoPlayer/popoutVideoPlayer/popoutVideoPlayer.ts
+++ src/static/scripts/components/mediaViewer/videoPlayer/popoutVideoPlayer/popoutVideoPlayer.ts
@@ -4,25 +4,31 @@ import Ph_SimpleVideo from "../simpleVideo/simpleVideo";
 import Ph_VideoAudio from "../videoAudio/videoAudio";
 import Ph_VideoPlayer from "../videoPlayer";
 import { BasicVideoData } from "../videoWrapper";
+import Users from "../../../multiUser/userManagement";
 
-const urlParams = new URLSearchParams(location.search)
-const data: BasicVideoData = JSON.parse(decodeURIComponent(urlParams.get("data")));
-const video = new Ph_VideoPlayer("");
-switch (data.className) {
-case "Ph_SimpleVideo":
-	video.init(new Ph_SimpleVideo([{ src: data.data, type: "video/mp4" }]));
-	break;
-case "Ph_VideoAudio":
-	const src = (data.data as string[]) ;
-	video.init(new Ph_VideoAudio(
-		[{ src: src[0], type: "video/mp4" }],
-		[{ src: src[1], type: "video/mp4" }]
-	));
-	break;
-case "Ph_GifVideo":
-	video.init(new Ph_GifVideo(data.data));
-	break;
+async function main() {
+	await Users.init();
+	const urlParams = new URLSearchParams(location.search)
+	const data: BasicVideoData = JSON.parse(decodeURIComponent(urlParams.get("data")));
+	const video = new Ph_VideoPlayer("");
+	switch (data.className) {
+		case "Ph_SimpleVideo":
+			video.init(new Ph_SimpleVideo([{ src: data.data, type: "video/mp4" }]));
+			break;
+		case "Ph_VideoAudio":
+			const src = (data.data as string[]) ;
+			video.init(new Ph_VideoAudio(
+				[{ src: src[0], type: "video/mp4" }],
+				[{ src: src[1], type: "video/mp4" }]
+			));
+			break;
+		case "Ph_GifVideo":
+			video.init(new Ph_GifVideo(data.data));
+			break;
+	}
+
+	const mediaViewer = new Ph_MediaViewer([video]);
+	document.body.append(mediaViewer);
 }
+main().catch(e => console.error(e));
 
-const mediaViewer = new Ph_MediaViewer([video]);
-document.body.append(mediaViewer);
